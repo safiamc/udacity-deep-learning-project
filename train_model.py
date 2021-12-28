eaed9896ce42f8151eb44a02258e54039bad949e
Safia Chettih <safiamcphd@gmail.com>
2021-12-28 23:02:19 +0000

    train_model copied from hpo

--- train_model.py
+++ train_model.py
@@ -9,73 +9,215 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 import argparse
+import logging
+import os
+import sys
+
+from smdebug import modes
+from smdebug.profiler.utils import str2bool
+from smdebug.pytorch import get_hook
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 #TODO: Import dependencies for Debugging andd Profiling
 
-def test(model, test_loader):
+def test(model, test_loader, criterion, hook):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    hook.register_loss(criterion)
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            loss = criterion(output, target)
+            test_loss += loss.item() * data.size(0)
+            pred = output.argmax(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            
 
-def train(model, train_loader, criterion, optimizer):
+    test_loss /= len(test_loader.dataset)
+    logger.info(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+def train(model, train_loader, validation_loader, criterion, optimizer, epochs, hook):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
-    
+    best_loss=1e6
+    image_dataset={'train':train_loader, 'valid':validation_loader}
+    loss_counter=0
+    hook.register_loss(criterion)
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase=='train':
+                model.train()
+                hook.set_mode(smd.modes.TRAIN)
+            else:
+                model.eval()
+                hook.set_mode(smd.modes.EVAL)
+            running_loss = 0.0
+            running_corrects = 0
+            running_samples=0
+
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase=='train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_samples+=len(inputs)
+                if running_samples % 2000  == 0:
+                    accuracy = running_corrects/running_samples
+                    print("Images [{}/{} ({:.0f}%)] Loss: {:.2f} Accuracy: {}/{} ({:.2f}%)".format(
+                            running_samples,
+                            len(image_dataset[phase].dataset),
+                            100.0 * (running_samples / len(image_dataset[phase].dataset)),
+                            loss.item(),
+                            running_corrects,
+                            running_samples,
+                            100.0*accuracy,
+                        )
+                    )
+            phase_loss = running_loss / running_samples
+            phase_acc = running_corrects / running_samples
+            logger.info(f"\nEpoch {epoch}, Phase {phase}")
+            logger.info(f"\nBest Loss: {best_loss:.4f}, Phase Loss: {phase_loss:.4f}, Phase Accuracy: {phase_acc:.4f}") 
+            if phase=='valid':
+                if epoch_loss<best_loss:
+                    best_loss=epoch_loss
+    return model
+  
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    model = models.resnet50(pretrained=True)
+    for param in model.parameters(): #freeze pretrained model weights
+        param.requires_grad = False
+        
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(nn.Linear(num_features, 200),
+                             nn.ReLU(),
+                             nn.Linear(200, 133),
+                             nn.Softmax()
+                            )
+    return model
 
 def create_data_loaders(data, batch_size):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
+    train_data_path = os.path.join(data, 'train')
+    validation_data_path=os.path.join(data, 'valid')
+    test_data_path=os.path.join(data, 'test')
+    
+    train_transform = transforms.Compose(
+        [transforms.RandomResizedCrop(224),
+         transforms.RandomRotation(45),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor()
+        ]
+    )
+    test_transform = transforms.Compose(
+        [transforms.Resize(224),
+         transforms.ToTensor()
+        ]
+    )
+    train_set = torchvision.datasets.ImageFolder(root=train_data_path, train=True, transform=train_transform)
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    valid_set = torchvision.datasets.ImageFolder(root=valid_data_path, train=False, transform = test_transform)
+    valid_loader = torch.utils.data.DataLoader(
+        valid_set,
+        batch_size=batch_size,
+        shuffle=False
+    )
+    test_set = torchvision.datasets.ImageFolder(root=test_data_path, train=False, transform = test_transform)
+    test_loader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=batch_size,
+        shuffle=False
+    )
+    return train_loader, validation_loader, test_loader
 
 def main(args):
     '''
     TODO: Initialize a model by calling the net function
     '''
     model=net()
-    
+    for key, value in vars(args).items():
+        logger.info(f"{key}:{value}")
+    hook=get_hook(create_if_not_exists=True)
+    hook.register_module(model)
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
-    
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr = args.lr )
+    batch_size=args.batch_size
+    epochs=args.epochs
+    train_loader, validation_loader, test_loader = create_data_loaders(args.data_dir, batch_size)
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    logger.info("Training the model")
+    model=train(model, train_loader, validation_loader, criterion, optimizer, epochs, hook)
     
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
+    logger.info("Testing the model")
+    test(model, test_loader, criterion, hook)
     
     '''
     TODO: Save the trained model
     '''
-    torch.save(model, path)
+    path = os.path.join(args.model_dir, "model.pth")
+    logger.info(f"Saving the model to path: {path}")
+    torch.save(model.state_dict(), path)
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
     '''
     TODO: Specify any training args that you might need
     '''
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--epochs", type=int, default=2)
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--model", type=str, default="resnet50")
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"] )
+    parser.add_argument("--data_dir", type=str, default= os.environ["SM_CHANNEL_TRAINING"])
+    
     
     args=parser.parse_args()
     
